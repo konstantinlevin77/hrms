0641148647cb30e3e231a96a9246d12b6e545e34
konstantinlevin <tekmanmehmetyasar@gmail.com>
2021-05-24 14:00:00 +0300

    Data access object of Jobseeker is written.

--- hrms/src/main/java/com/konstantinlevin77/hrms/dataAccess/abstracts/JobseekerDao.java
+++ hrms/src/main/java/com/konstantinlevin77/hrms/dataAccess/abstracts/JobseekerDao.java
@@ -1,5 +1,9 @@
 package com.konstantinlevin77.hrms.dataAccess.abstracts;
 
-public interface JobseekerDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.konstantinlevin77.hrms.entities.concretes.Jobseeker;
+
+public interface JobseekerDao extends JpaRepository<Jobseeker,Integer>{
 
 }
