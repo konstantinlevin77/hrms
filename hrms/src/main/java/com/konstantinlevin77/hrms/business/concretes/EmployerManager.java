3056a964ce74ead273691cf3ba2fb1311de10a3e
konstantinlevin <tekmanmehmetyasar@gmail.com>
2021-05-27 12:04:40 +0300

    verify method of EmployerService is written.

--- hrms/src/main/java/com/konstantinlevin77/hrms/business/concretes/EmployerManager.java
+++ hrms/src/main/java/com/konstantinlevin77/hrms/business/concretes/EmployerManager.java
@@ -10,6 +10,7 @@ import com.konstantinlevin77.hrms.business.concretes.employerFieldCheckers.Emplo
 import com.konstantinlevin77.hrms.business.concretes.employerFieldCheckers.EmployerEmailChecker;
 import com.konstantinlevin77.hrms.core.results.abstracts.DataResult;
 import com.konstantinlevin77.hrms.core.results.abstracts.Result;
+import com.konstantinlevin77.hrms.core.results.concretes.ErrorResult;
 import com.konstantinlevin77.hrms.core.results.concretes.SuccessDataResult;
 import com.konstantinlevin77.hrms.core.results.concretes.SuccessResult;
 import com.konstantinlevin77.hrms.dataAccess.abstracts.EmployerDao;
@@ -59,6 +60,20 @@ public class EmployerManager implements EmployerService{
 			return domainChecker.check(employer, this.employerDao);
 		}
 		
+	}
+
+	@Override
+	public Result verify(int id) {
+		Employer employer = this.employerDao.findById(id).get();
+		
+		if(employer.isEmailVerified()) {
+			return new ErrorResult("Bu hesabın maili zaten onaylanmış!");
+		}
+		
+		employer.setEmailVerified(true);
+		this.employerDao.save(employer);
+		
+		return new SuccessResult("Bu hesabın maili başarı ile onaylandı!");
 	}
 	
 	
