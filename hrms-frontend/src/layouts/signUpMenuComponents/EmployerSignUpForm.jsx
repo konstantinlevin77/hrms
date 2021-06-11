fc0df3c3cc005e6bc6bbcf45f0ca6ba6d51e62b3
konstantinlevin <tekmanmehmetyasar@gmail.com>
2021-06-11 17:48:09 +0300

    JobseekerSignUpForm.jsx and EmployerSignUpForm.jsx are initialized. They'll be implemented soon.

---
+++ hrms-frontend/src/layouts/signUpMenuComponents/EmployerSignUpForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EmployerSignUpForm() {
+    return (
+        <div>
+            {/* Not implemented yet, I'll implement them with formik.*/}
+        </div>
+    )
+}
---
+++ hrms-frontend/src/layouts/signUpMenuComponents/JobseekerSignUpForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function JobseekerSignUpForm() {
+    return (
+        <div>
+            {/* Not implemented yet, I'll implement them with formik.*/}
+        </div>
+    )
+}
