135746d8308301e52ce5f93809321fd2dd8edfe5
konstantinlevin <tekmanmehmetyasar@gmail.com>
2021-06-14 22:08:58 +0300

    Sign In form is implemented using Formik, some style improvements.

--- hrms-frontend/src/pages/SignInMenu.jsx
+++ hrms-frontend/src/pages/SignInMenu.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
-import { Form } from 'semantic-ui-react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import styles from '../styles/SignInMenuFormStyles.module.css'
 
 export default function SignInMenu() {
     return (
         <div>
             <div style={{ display: 'flex', margin: '20px', padding: '20px', justifyContent: 'center', border: '1px solid black', width: '600', height: '600', borderRadius: '5px' }}>
                 <div style={{ width: "40%", height: "60%", justifyContent: "center", display: 'flex', marginTop: '10px', margin: '40px' }}>
-                    <Form>
-                        <h3>It's nice to see you again!</h3>
-                        <Form.Field>
-                            <label>Email</label>
-                            <input placeholder="Type your email"></input>
-                        </Form.Field>
-                        <Form.Field>
-                            <label>Password</label>
-                            <input type="password" placeholder="Type your password"/>
-                        </Form.Field>
-                        <Form.Button primary>Sign In</Form.Button>
-                    </Form>
+                    {/* For now, I've disabled validations but I'll get them later.*/}
+                    <Formik
+                        initialValues={{ email: '', password: '' }}
+
+                        onSubmit={(values, { setSubmitting }) => {
+                            setTimeout(() => {
+                                alert(JSON.stringify(values, null, 2));
+                                setSubmitting(false);
+                            }, 400);
+                        }}
+                    >
+                        {({ isSubmitting }) => (
+                            <Form>
+                                <label>Email</label>
+                                <Field type="email" name="email" className={styles.field}/>
+                                <ErrorMessage name="email" component="div" />
+                                <label>Pasword</label>
+                                <Field type="password" name="password" className={styles.field}/>
+                                <ErrorMessage name="password" component="div" />
+                                <button type="submit" className={styles.button} disabled={isSubmitting}>
+                                    Submit
+                                </button>
+                            </Form>
+                        )}
+                    </Formik>
                 </div>
             </div>
         </div>
