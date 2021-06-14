ef49bb68677c5a5a555fad178a4dd2945f9d3a2c
konstantinlevin <tekmanmehmetyasar@gmail.com>
2021-06-14 21:23:58 +0300

    Style problems of Footer.jsx is solved.

--- hrms-frontend/src/layouts/Footer.jsx
+++ hrms-frontend/src/layouts/Footer.jsx
@@ -4,7 +4,13 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div class="ui inverted vertical footer segment" style={{ bottom: 0, width: '100%' }}>
+        <div class="ui inverted vertical footer segment" style={{
+            position: "absolute",
+            bottom: 0,
+            width: "100 %",
+            height: "100px"
+        }}>
+
             <div class="ui container">
                 <p>Human Resources Management System Project</p>
                 <p>This project is completely under MIT license.</p>
